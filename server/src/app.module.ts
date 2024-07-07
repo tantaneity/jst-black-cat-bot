6e93ace35f75d93f7f5335627fdccb971d9e3161
ipz222mno <ipz222_mno@student.ztu.edu.ua>
2024-07-08 00:15:19 +0300

    feat: Implement comprehensive UserRepository for User entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { APP_FILTER } from '@nestjs/core';
+import { UserModule } from './modules/user.module';
+import { HttpExceptionFilter } from 'utils/error/http-exception.filter';
+import { DatabaseModule } from './modules/utils/database.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, DatabaseModule], //TODO: Find a better way like BaseModule or sum like that
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    }
+  ]
 })
 export class AppModule {}
