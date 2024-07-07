90907d391c838cb3ad6a6bd82cd15c3fbe12ee7b
ipz222mno <ipz222_mno@student.ztu.edu.ua>
2024-07-08 01:38:25 +0300

    dev: Added config for the db module

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Get, Post } from "@nestjs/common";
 
 import { UserService } from "../services/user.service";
 import { CreateUserDto } from "../models/dto/user/create-user.dto";
+import { Public } from "utils/decorators/public.decorator";
 
 @Controller('users')
 export class UserController {
     constructor(private service: UserService) {}
 
+    @Public()
     @Get()
     getUsers() {
         return this.service.getUsers()
--- server/src/modules/utils/database.module.ts
+++ server/src/modules/utils/database.module.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { UserEntity } from 'src/models/entities/user.entity';
 
 @Module({
     imports: [TypeOrmModule.forRootAsync({
@@ -9,12 +10,12 @@ import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
         useFactory: (configService: ConfigService) => {
             const dbConfig: TypeOrmModuleOptions = {
                 type: 'postgres',
-                host: 'localhost',
-                port: configService.get<number>('PG_PORT'),
-                username: configService.get<string>('PG_USERNAME'),
-                password: configService.get<string>('PG_PASSWORD'),
-                database: 'JstBlackCatBotDB',
-                entities: [__dirname + '/../../../models/entities/*.entity.{js,ts}'],
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+                entities: [UserEntity],
                 synchronize: true,
                 cache: true
             };
