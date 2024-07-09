d378b5c595182a38691bf62c089eae354a04b262
ipz222mno <ipz222_mno@student.ztu.edu.ua>
2024-07-09 07:24:48 +0300

    dev: Created a basic template for interaction between the NestJS server and the bot on Pyrogram

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { UserService } from "../services/user.service";
 import { CreateUserDto } from "../models/dto/user/create-user.dto";
 import { Public } from "utils/decorators/public.decorator";
+import { UserEntity } from "src/models/entities/user.entity";
 
 @Controller('users')
 export class UserController {
@@ -11,13 +11,19 @@ export class UserController {
     @Public()
     @Get()
     getUsers() {
-        return this.service.getUsers()
+        return this.service.getUsers();
+    }
+
+    @Public()
+    @Get(':id')
+    getUser(@Param('id') id: number) {
+        const user = this.service.getUser({ user_id: id });
+        return user;
     }
 
     @Post('create')
     create(@Body() user: CreateUserDto) {
-        this.service.create(user)
-
-        return { message: 'User was successfully created' }
+        this.service.create(user);
+        return { message: 'User was successfully created' };
     }
 }
